dac6c107e5e0722daecbfbe7e07a038af49b18af
Alex <nalexcastaneda@gmail.com>
2021-01-15 14:45:58 -0600

    Updated contact links and projects section

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ const slideNav = () => {
     //Toggle navBar
     burger.addEventListener('click', () => {
         nav.classList.toggle('active-nav');
-            //Links animation
-    navLinks.forEach((link, index) => {
-        link.style.animation = `navLinksAnimation 0.5s ease forwards ${index / 7}s`;
-        console.log(index / 7 + 1);
-    });
+        //Links animation
+        navLinks.forEach((link, index) => {
+            link.style.animation = `navLinksAnimation 0.5s ease forwards ${index / 7}s`;
+            console.log(index / 7 + 1);
+        });
     });
 }
 
